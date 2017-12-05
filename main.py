8696010d559b195b5f7e691d97d4cc875643fe57
guider <guider@yeah.net>
2017-12-05 12:26:29 +0800

    gif

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from sql_helper import MySQLClient
 import time
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) '
-                  + 'Mobile/14G60 MicroMessenger/6.5.23 NetType/WIFI Language/zh_CN',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
     'Referer': 'https://servicewechat.com/wx38be548c581f8c61/10/page-frame.html',
-    'Content-Type': 'application/json',
-    'X-AspNet-Version': '4.0.30319',
-    'X-Powered-By': 'ASP.NET'
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,la;q=0.6',
+    'If-None-Match': '"3ce9f2-4a29c-548f423b77900"',
+    'If-Modified-Since': 'Mon, 20 Feb 2017 10:58:12 GMT'
 }
 
 url = 'https://rayscloud.chubanyun.net/api/Writing/WritingCommon/writingBiShunByWords'
@@ -48,24 +52,27 @@ def getLetters():
         return letters
 
 
-def downloadImage(imgid):
-    imgurl = baseImageUrl + imgid + '.png'
+def downloadImage(txt):
+    imgurl = 'https://xinhuazidian.18dao.cn/bishun/'+txt+'.gif'
+    print(imgurl)
     from contextlib import closing
     with closing(requests.get(imgurl, headers=headers, stream=True)) as response:
-        with open('./images/' + imgid + '.png', 'wb') as fd:
+        with open('./gifs/' + txt + '.gif', 'wb') as fd:
             for chunk in response.iter_content(256):
                 fd.write(chunk)
 
 
 if __name__ == '__main__':
-    for index, item in enumerate(getLetters()[2400:]):
+    for index, item in enumerate(getLetters()):
         print(item)
 
         try:
             print(item)
             if item.strip() == '':
                 continue
-            fetchData(item)
+
+            downloadImage(item)
+            # fetchData(item)
             time.sleep(0.5)
         except BaseException:
             print('error')
